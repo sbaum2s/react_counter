8d7c3581e1c4cd31271c5a3ba93957816c61e88a
sbaum2s <80587859+sbaum2s@users.noreply.github.com>
2021-11-04 14:27:12 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React, {useState} from "react"
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button";
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = increment => {
+    setCount(count + increment)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button increment={1} onClickFunction={incrementCount}/>
+      <Button increment={10} onClickFunction={incrementCount}/>
+      <Button increment={100} onClickFunction={incrementCount}/>
+      <Button increment={1000} onClickFunction={incrementCount}/>
+      <span>{count}</span>
     </div>
   );
 }
